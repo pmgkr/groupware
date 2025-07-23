163f31e30d872438e54eabf8b66e4904a22a6e4e
yeaji.kim <83742940+yeajikim@users.noreply.github.com>
2025-07-23 13:21:30 +0900

    :design: Pretendard font-face 추가
    Pretendard font-face 추가및 Board Pges 제거

--- client/src/router/index.tsx
+++ client/src/router/index.tsx
@@ -2,9 +2,6 @@
 import { createBrowserRouter } from 'react-router';
 import Login from '@/pages/Login';
 import Dashboard from '@/pages/Dashboard';
-import BoardList from '@/pages/board/BoardList';
-import BoardDetail from '@/pages/board/BoardDetail';
-import BoardWrite from '@/pages/board/BoardWrite';
 import ErrorPage from '@/pages/ErrorPage';
 
 export const router = createBrowserRouter([
@@ -15,18 +12,6 @@ export const router = createBrowserRouter([
   {
     path: '/dashboard',
     element: <Dashboard />,
-  },
-  {
-    path: '/board/list',
-    element: <BoardList />,
-  },
-  {
-    path: '/board/detail/:postId',
-    element: <BoardDetail />,
-  },
-  {
-    path: '/board/write',
-    element: <BoardWrite />,
   },
   {
     path: '*',
