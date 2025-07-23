8bbf0a4946a3428c74ff2fdcf668aef82c74d1ee
yeaji.kim <83742940+yeajikim@users.noreply.github.com>
2025-07-23 13:02:48 +0900

    remove: 사용하지 않는 utils 파일 제거

--- client/src/utils/shopUtils.ts
+++
@@ -1,5 +0,0 @@
-export function formatPrice(price: number | string): string {
-  const numericPrice = typeof price === 'string' ? parseFloat(price) : price;
-  if (isNaN(numericPrice)) return '-';
-  return numericPrice.toLocaleString('ko-KR');
-}
--- client/src/utils/utils.ts
+++ client/src/utils/utils.ts
@@ -1,3 +1,11 @@
 export function isNum(obj: string | number): boolean {
-  return !isNaN(Number(obj)); // 숫자면 true, 문자면 false
+  // 숫자면 true, 문자면 false
+  return !isNaN(Number(obj));
+}
+
+export function formatAmount(amount: number | string): string {
+  // 원화일 때 1000원 단위로 콤마 추가
+  const numericAmount = typeof amount === 'string' ? parseFloat(amount) : amount;
+  if (isNaN(numericAmount)) return '-';
+  return numericAmount.toLocaleString('ko-KR');
 }
