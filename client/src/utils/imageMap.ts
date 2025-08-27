bce824d98a2ded2c9b4ac66188d15f0b2f74c24d
yeaji.kim <83742940+yeajikim@users.noreply.github.com>
2025-08-27 13:46:57 +0900

    fix: imageMap 코드 수정

--- client/src/utils/imageMap.ts
+++ client/src/utils/imageMap.ts
@@ -1,12 +1,22 @@
-// 이미지 맵핑 유틸 함수
 const modules = import.meta.glob('../assets/images/**/*.{png,jpg,gif,webp,svg,mp4}', {
   eager: true,
   import: 'default',
 });
-const basename = (path: string) => path.split('/').pop()!;
+
+// ../assets/images/ 이후의 경로만 추출
+const relativePath = (path: string) => path.replace(/^.*\/assets\/images\//, '');
+// 확장자 제거 유틸
+const removeExt = (filename: string) => filename.replace(/\.[^/.]+$/, '');
 
 export const imageMap: Record<string, string> = Object.fromEntries(
-  Object.entries(modules).map(([path, url]) => [basename(path), url as string])
+  Object.entries(modules).flatMap(([path, url]) => {
+    const relPath = relativePath(path); // common/logo.png
+    const noExt = removeExt(relPath); // common/logo
+    return [
+      [relPath, url as string], // 확장자 포함 키
+      [noExt, url as string], // 확장자 없는 키
+    ];
+  })
 );
 
 export function getImageUrl(filename: string) {
