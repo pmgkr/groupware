d1ec2504d75f1ce26f4c70365c90f3c9073c4382
yeaji.kim <83742940+yeajikim@users.noreply.github.com>
2025-09-01 18:15:35 +0900

    fix: 파일명 수정

--- client/src/Main.tsx
+++ client/src/Main.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router';
-import { router } from './router/';
+import { router } from './router';
 import '@/index.css';
 
 createRoot(document.getElementById('root')!).render(
