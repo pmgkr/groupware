ed9cf1d889888e108b6619c88c440ef86a64b8df
Yeonsang Lee <120547797+pmgyeonsang@users.noreply.github.com>
2025-10-24 11:54:45 +0900

    feat: 캘린더 내 일정만 확인 가능한 nav 추가

--- client/src/router/calendar/index.tsx
+++ client/src/router/calendar/index.tsx
@@ -4,7 +4,13 @@ import Calendar from '@/pages/Calendar/';
 export const calendarRoutes: RouteObject = {
   handle: {
     title: '캘린더',
-    nav: [{ to: '/calendar', label: '전체', end: true }],
+    nav: [
+      { to: '/calendar', label: '전체', end: true },
+      { to: '/calendar/my', label: '내 일정', end: true }
+    ],
   },
-  children: [{ path: 'calendar', element: <Calendar /> }],
+  children: [
+    { path: 'calendar', element: <Calendar /> },
+    { path: 'calendar/my', element: <Calendar filterMyEvents={true} /> }
+  ],
 };
