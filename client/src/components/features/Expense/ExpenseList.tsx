1a53f362ab0718c851479a4cd1f4b46d78e6e7de
yeaji.kim <83742940+yeajikim@users.noreply.github.com>
2025-10-24 14:06:52 +0900

    feat: 엑셀 업로드 기능 추가

--- client/src/components/features/Expense/ExpenseList.tsx
+++ client/src/components/features/Expense/ExpenseList.tsx
@@ -1,6 +1,8 @@
+import { useRef, useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router';
-import { useRef, useState } from 'react';
 import * as XLSX from 'xlsx';
+import { useUser } from '@/hooks/useUser';
+import { formatKST, formatAmount } from '@/utils';
 
 import { Button } from '@components/ui/button';
 import { Badge } from '@components/ui/badge';
@@ -9,40 +11,44 @@ import { Select, SelectTrigger, SelectValue, SelectContent, SelectGroup, SelectI
 import { Dialog, DialogClose, DialogDescription, DialogContent, DialogHeader, DialogTitle, DialogFooter } from '@/components/ui/dialog';
 import { MultiSelect, type MultiSelectOption } from '@components/multiselect/multi-select';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
-import { boolean } from 'zod';
+import { Excel } from '@/assets/images/icons';
+
+import { getExpenseLists, type ExpenseListItem, getExpenseType } from '@/api';
 
 export default function ExpenseList() {
   const navigate = useNavigate();
+  const { user_id, user_level } = useUser();
 
+  // 상단 필터용 state
   const [selectedType, setSelectedType] = useState<string[]>([]);
   const [selectedStatus, setSelectedStatus] = useState<string[]>([]);
   const [selectedProof, setSelectedProof] = useState<string[]>([]);
   const [selectedProofStatus, setSelectedProofStatus] = useState<string[]>([]);
   const [registerDialog, setRegisterDialog] = useState(false);
 
-  // Excel 데이터 저장용
-  const [excelData, setExcelData] = useState<any[]>([]);
+  // API 데이터 state
+  const [typeOptions, setTypeOptions] = useState<MultiSelectOption[]>([]);
+  const [expenseList, setExpenseList] = useState<ExpenseListItem[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  // Excel 데이터 업로드용 Input
   const fileInputRef = useRef<HTMLInputElement | null>(null);
 
   // Excel 파일 업로드 핸들러
-  const handleExcelUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleExcelUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (!file) return;
 
-    const reader = new FileReader();
-    reader.onload = (evt) => {
-      const data = evt.target?.result;
-      const workbook = XLSX.read(data, { type: 'binary' });
-      const sheet = workbook.Sheets[workbook.SheetNames[0]];
-      const jsonData = XLSX.utils.sheet_to_json(sheet, { defval: '' });
+    const data = await file.arrayBuffer();
+    const workbook = XLSX.read(data, { type: 'array' });
+    const sheetName = workbook.SheetNames[0];
+    const worksheet = workbook.Sheets[sheetName];
+    const jsonData = XLSX.utils.sheet_to_json(worksheet);
 
-      console.log('✅ 업로드된 Excel 데이터:', jsonData);
-      setExcelData(jsonData);
+    console.log('✅ 업로드된 Excel 데이터:', jsonData);
 
-      // 업로드 완료 후 register 페이지로 이동
-      navigate('/expense/register', { state: { excelData: jsonData } });
-    };
-    reader.readAsBinaryString(file);
+    // 업로드 완료 후 register 페이지로 이동
+    navigate('/expense/register', { state: { excelData: jsonData } });
   };
 
   // 엑셀 업로드 버튼 클릭 시 input 트리거
@@ -50,21 +56,12 @@ export default function ExpenseList() {
     fileInputRef.current?.click();
   };
 
-  // 페이지네이션
-  const [total, setTotal] = useState(20);
+  // 리스트 페이지네이션
+  const [total, setTotal] = useState(0);
   const [page, setPage] = useState(1);
-
   const pageSize = 15; // 한 페이지에 보여줄 개수
 
   // 필터 옵션 정의
-  const typeOptions: MultiSelectOption[] = [
-    { label: '야근식대', value: '야근식대' },
-    { label: '야근교통비', value: '야근교통비' },
-    { label: '소모품비', value: '소모품비' },
-    { label: '소모품비2', value: '소모품비2' },
-    { label: '소모품비3', value: '소모품비3' },
-  ];
-
   const statusOptions: MultiSelectOption[] = [
     { label: '임시저장', value: '임시저장' },
     { label: '승인대기', value: '승인대기' },
@@ -74,17 +71,56 @@ export default function ExpenseList() {
   ];
 
   const proofMethod: MultiSelectOption[] = [
-    { label: 'PMG 법인', value: 'PMG 법인' },
-    { label: 'MCS 법인', value: 'MCS 법인' },
+    { label: 'PMG', value: 'PMG' },
+    { label: 'MCS', value: 'MCS' },
     { label: '개인카드', value: '개인카드' },
     { label: '세금계산서', value: '세금계산서' },
     { label: '현금영수증', value: '현금영수증' },
+    { label: '기타', value: '기타' },
   ];
 
   const proofStatusOptions: MultiSelectOption[] = [
     { label: '제출', value: '제출' },
     { label: '미제출', value: '미제출' },
   ];
+
+  // 비용 유형 가져오기
+  useEffect(() => {
+    (async () => {
+      try {
+        // 유저레벨이 staff나 user인 경우 nexp_type2 : manager나 admin인 경우 nexp_type1 호출
+        const expenseTypeParam = user_level === 'staff' || user_level === 'user' ? 'nexp_type2' : 'nexp_type1';
+
+        const data = await getExpenseType(expenseTypeParam);
+        const mapped = data.map((t: any) => ({
+          label: t.code,
+          value: t.code,
+        }));
+        setTypeOptions(mapped);
+      } catch (err) {
+        console.error('❌ 비용유형 불러오기 실패:', err);
+      }
+    })();
+  }, []);
+
+  // 비용 리스트 가져오기
+  useEffect(() => {
+    (async () => {
+      try {
+        setLoading(true);
+
+        const res = await getExpenseLists();
+        console.log('✅ 비용 리스트:', res);
+
+        setExpenseList(res.items);
+        setTotal(res.total);
+      } catch (err) {
+        console.error('❌ 비용 리스트 불러오기 실패:', err);
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, []);
 
   return (
     <>
@@ -186,196 +222,99 @@ export default function ExpenseList() {
         </Button>
       </div>
 
-      <Table variant="primary" align="left">
+      <Table variant="primary" align="center">
         <TableHeader>
-          <TableRow>
-            <TableHead className="w-[10%]">EXP#</TableHead>
-            <TableHead className="w-[9%]">비용 용도</TableHead>
-            <TableHead className="text-center">비용 제목</TableHead>
-            <TableHead className="w-[8%]">증빙 수단</TableHead>
+          <TableRow className="[&_th]:text-[13px] [&_th]:font-medium">
+            <TableHead className="w-[6%]">EXP#</TableHead>
+            <TableHead className="w-[6%]">증빙 수단</TableHead>
+            <TableHead className="w-[8%]">비용 용도</TableHead>
+            <TableHead>비용 제목</TableHead>
             <TableHead className="w-[8%]">증빙 상태</TableHead>
-            <TableHead className="w-[10%]">합계 금액</TableHead>
-            <TableHead className="w-[8%]">상태</TableHead>
-            <TableHead className="w-[12%]">작성자</TableHead>
-            <TableHead className="w-[16%]">작성 일시</TableHead>
+            <TableHead className="w-[10%]">금액</TableHead>
+            <TableHead className="w-[6%]">세금</TableHead>
+            <TableHead className="w-[10%]">합계</TableHead>
+            <TableHead className="w-[6%]">상태</TableHead>
+            <TableHead className="w-[14%]">작성 일시</TableHead>
           </TableRow>
         </TableHeader>
 
         <TableBody>
-          {/* TODO: 여기에 DB 데이터 매핑 */}
-          <TableRow>
-            <TableCell>
-              <Link to="/expense/1" className="rounded-[4px] border-1 bg-white p-1 text-sm">
-                PN25-27564
-              </Link>
-            </TableCell>
-            <TableCell>야근교통비</TableCell>
-            <TableCell>야근교통비 3건</TableCell>
-            <TableCell>
-              <Badge variant="outline" size="table" className="border-gray-300 text-gray-700">
-                현금영수증
-              </Badge>
-            </TableCell>
-            <TableCell>
-              <Badge variant="secondary" size="table">
-                제출
-              </Badge>
-            </TableCell>
-            <TableCell>45,000원</TableCell>
-            <TableCell>
-              <Badge variant="secondary">승인대기</Badge>
-            </TableCell>
-            <TableCell>홍길동</TableCell>
-            <TableCell>2025-07-04 14:44:00</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>
-              <Link to="/expense/1" className="rounded-[4px] border-1 bg-white p-1 text-sm">
-                PN25-27564
-              </Link>
-            </TableCell>
-            <TableCell>야근교통비</TableCell>
-            <TableCell>야근교통비 3건</TableCell>
-            <TableCell>
-              <Badge variant="outline" size="table" className="border-gray-300 text-gray-700">
-                PMG 법인
-              </Badge>
-            </TableCell>
-            <TableCell>
-              <Badge variant="secondary" size="table">
-                제출
-              </Badge>
-            </TableCell>
-            <TableCell>45,000원</TableCell>
-            <TableCell>
-              <Badge variant="secondary">승인대기</Badge>
-            </TableCell>
-            <TableCell>홍길동</TableCell>
-            <TableCell>2025-07-04 14:44:00</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>
-              <Link to="/expense/1" className="rounded-[4px] border-1 bg-white p-1 text-sm">
-                PN25-27564
-              </Link>
-            </TableCell>
-            <TableCell>야근교통비</TableCell>
-            <TableCell>야근교통비 3건</TableCell>
-            <TableCell>
-              <Badge variant="outline" size="table" className="border-gray-300 text-gray-700">
-                PMG 법인
-              </Badge>
-            </TableCell>
-            <TableCell>
-              <Badge variant="secondary" size="table">
-                제출
-              </Badge>
-            </TableCell>
-            <TableCell>45,000원</TableCell>
-            <TableCell>
-              <Badge variant="secondary">승인대기</Badge>
-            </TableCell>
-            <TableCell>홍길동</TableCell>
-            <TableCell>2025-07-04 14:44:00</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>
-              <Link to="/expense/1" className="rounded-[4px] border-1 bg-white p-1 text-sm">
-                PN25-27564
-              </Link>
-            </TableCell>
-            <TableCell>야근교통비</TableCell>
-            <TableCell>야근교통비 3건</TableCell>
-            <TableCell>
-              <Badge variant="outline" size="table" className="border-gray-300 text-gray-700">
-                PMG 법인
-              </Badge>
-            </TableCell>
-            <TableCell>
-              <Badge variant="secondary" size="table">
-                제출
-              </Badge>
-            </TableCell>
-            <TableCell>45,000원</TableCell>
-            <TableCell>
-              <Badge variant="secondary">승인대기</Badge>
-            </TableCell>
-            <TableCell>홍길동</TableCell>
-            <TableCell>2025-07-04 14:44:00</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>
-              <Link to="/expense/1" className="rounded-[4px] border-1 bg-white p-1 text-sm">
-                PN25-27564
-              </Link>
-            </TableCell>
-            <TableCell>동아리비</TableCell>
-            <TableCell>동아리비 2건</TableCell>
-            <TableCell>
-              <Badge variant="outline" size="table" className="border-gray-300 text-gray-700">
-                현금영수증
-              </Badge>
-            </TableCell>
-            <TableCell>
-              <Badge variant="secondary" size="table">
-                제출
-              </Badge>
-            </TableCell>
-            <TableCell>45,000원</TableCell>
-            <TableCell>
-              <Badge variant="secondary">승인대기</Badge>
-            </TableCell>
-            <TableCell>홍길동</TableCell>
-            <TableCell>2025-07-04 14:44:00</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>
-              <Link to="/expense/1" className="rounded-[4px] border-1 bg-white p-1 text-sm">
-                PN25-27564
-              </Link>
-            </TableCell>
-            <TableCell>야근교통비</TableCell>
-            <TableCell>야근교통비 3건</TableCell>
-            <TableCell>
-              <Badge variant="outline" size="table" className="border-gray-300 text-gray-700">
-                개인카드
-              </Badge>
-            </TableCell>
-            <TableCell>
-              <Badge variant="grayish" size="table">
-                미제출
-              </Badge>
-            </TableCell>
-            <TableCell>45,000원</TableCell>
-            <TableCell>
-              <Badge variant="secondary">승인대기</Badge>
-            </TableCell>
-            <TableCell>홍길동</TableCell>
-            <TableCell>2025-07-04 14:44:00</TableCell>
-          </TableRow>
+          {loading ? (
+            <TableRow>
+              <TableCell className="h-100 text-gray-500" colSpan={10}>
+                비용 리스트 불러오는 중 . . .
+              </TableCell>
+            </TableRow>
+          ) : expenseList.length === 0 ? (
+            <TableRow>
+              <TableCell className="h-100 text-gray-500" colSpan={10}>
+                리스트가 없습니다.
+              </TableCell>
+            </TableRow>
+          ) : (
+            expenseList.map((item) => {
+              // 비용 상태값 매핑
+              const statusMap = {
+                Saved: <Badge variant="grayish">임시저장</Badge>,
+                Claimed: <Badge variant="secondary">승인대기</Badge>,
+                Confirmed: <Badge>승인완료</Badge>,
+                Completed: <Badge className="bg-primary-blue">지급완료</Badge>,
+                Rejected: <Badge className="bg-destructive">반려됨</Badge>,
+              };
+
+              const status = statusMap[item.status as keyof typeof statusMap];
+
+              return (
+                <TableRow key={item.seq} className="[&_td]:text-[13px]">
+                  <TableCell>
+                    <Link to={`/expense/${item.exp_id}`} className="rounded-[4px] border-1 bg-white p-1 text-sm">
+                      {item.exp_id}
+                    </Link>
+                  </TableCell>
+                  <TableCell>{item.el_method}</TableCell>
+                  <TableCell>{item.el_type}</TableCell>
+                  <TableCell className="text-left">
+                    <Link to={`/expense/${item.exp_id}`} className="hover:underline">
+                      {item.el_title}
+                    </Link>
+                  </TableCell>
+                  <TableCell>
+                    {item.el_attach === 'Y' ? <Badge variant="secondary">제출</Badge> : <Badge variant="grayish">미제출</Badge>}
+                  </TableCell>
+                  <TableCell>{formatAmount(item.el_amount)}원</TableCell>
+                  <TableCell>{item.el_tax === 0 ? 0 : `${formatAmount(item.el_tax)}원`}</TableCell>
+                  <TableCell>{formatAmount(item.el_total)}원</TableCell>
+                  <TableCell>{status}</TableCell>
+                  <TableCell>{formatKST(item.wdate)}</TableCell>
+                </TableRow>
+              );
+            })
+          )}
         </TableBody>
       </Table>
 
       <div className="mt-5">
-        <AppPagination
-          totalPages={Math.ceil(total / pageSize)}
-          initialPage={page}
-          visibleCount={5}
-          onPageChange={(p) => setPage(p)} //부모 state 업데이트
-        />
+        {expenseList.length !== 0 && (
+          <AppPagination
+            totalPages={Math.ceil(total / pageSize)}
+            initialPage={page}
+            visibleCount={5}
+            onPageChange={(p) => setPage(p)} //부모 state 업데이트
+          />
+        )}
       </div>
 
       <Dialog open={registerDialog} onOpenChange={setRegisterDialog}>
         <DialogContent>
           <DialogHeader>
             <DialogTitle>신규 비용 등록</DialogTitle>
-            <DialogDescription>비용을 수기로 입력하거나, Excel 파일을 업로드해 자동으로 입력할 수 있습니다.</DialogDescription>
+            <DialogDescription>매입 내역 Excel 파일을 업로드해 데이터를 불러오거나 수기로 입력할 수 있습니다.</DialogDescription>
           </DialogHeader>
           <div className="space-y-3 py-4">
             <p className="text-base">등록하실 비용 유형을 선택해주세요.</p>
             <div className="grid grid-cols-2 gap-4">
               <Button variant="outline" onClick={openFileDialog}>
-                Excel 업로드
+                <Excel className="size-4.5" /> Excel 업로드
               </Button>
               <Button variant="outline" asChild>
                 <Link to="/expense/register">수기 입력</Link>
@@ -383,11 +322,6 @@ export default function ExpenseList() {
             </div>
             <input ref={fileInputRef} type="file" accept=".xlsx, .xls" className="h-0 w-0 text-[0]" onChange={handleExcelUpload} />
           </div>
-          <DialogFooter>
-            <DialogClose asChild>
-              <Button variant="outline">닫기</Button>
-            </DialogClose>
-          </DialogFooter>
         </DialogContent>
       </Dialog>
     </>
