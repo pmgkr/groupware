6ed77c9e62044e7eb84603afa89628336d4aa55c
Yeonsang Lee <120547797+pmgyeonsang@users.noreply.github.com>
2025-11-03 14:49:18 +0900

    design: 출퇴근관리 구분 색상 변경

--- client/src/components/working/table.tsx
+++ client/src/components/working/table.tsx
@@ -28,12 +28,12 @@ export default function Table({ data, onDataRefresh }: TableProps) {
       case "-": return "bg-gray-50 text-gray-400";
       case "일반근무": return "bg-gray-300 text-gray-900";
       case "연차": return "bg-primary-blue-150 text-primary-blue";
-      case "오전반차": return "bg-primary-pink-300 text-primary-pink-500";
-      case "오전반반차": return "bg-primary-purple-150 text-primary-purple-500";
-      case "오후반차": return "bg-primary-pink-300 text-primary-pink-500";
-      case "오후반반차": return "bg-primary-purple-150 text-primary-purple-500";
+      case "오전반차": return "bg-primary-purple-100 text-primary-pink-500";
+      case "오전반반차": return "bg-primary-purple-100 text-primary-purple-500";
+      case "오후반차": return "bg-primary-purple-100 text-primary-pink-500";
+      case "오후반반차": return "bg-primary-purple-100 text-primary-purple-500";
       case "외부근무": return "bg-primary-yellow-150 text-primary-orange-600";
-      case "재택근무": return "bg-primary-blue-100 text-primary-blue-600";
+      case "재택근무": return "bg-gray-300 text-gray-900";
       case "공가": return "bg-red-100 text-red-600";
       case "공휴일": return "bg-red-200 text-red-700";
       default: return "bg-primary-gray-100 text-primary-gray";
