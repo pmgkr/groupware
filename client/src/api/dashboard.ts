81a461a7fe85655a38be9c63a5362fdeaa09fd81
Yeonsang Lee <120547797+pmgyeonsang@users.noreply.github.com>
2025-11-26 18:01:47 +0900

    feat: 대시보드 출퇴근시간 api 관련 수정

--- client/src/api/dashboard.ts
+++ client/src/api/dashboard.ts
@@ -5,11 +5,15 @@ import { http } from '@/lib/http';
 export interface Wlog {
     wlogWeek: {
        user_id: string,
-       total_minutes:number // 총 근무시간 
+       total_minutes:number, // 총 근무시간 
+       whour:number, // 총 근무시간 시간
+       wmin:number
     }[];
     wlogToday: {
         stime: string | null,
-        etime: string | null
+        etime: string | null,
+        c_stime: string | null,
+        c_etime: string | null,
     }[];
 }
 
