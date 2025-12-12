6f93058bf5e8e57b382eda2945ee9a916c1fa0ab
yeaji.kim <83742940+yeajikim@users.noreply.github.com>
2025-12-12 10:24:45 +0900

    feat: 프로젝트 비용 추가

--- client/src/components/features/Project/ManagerExpenseList.tsx
+++ client/src/components/features/Project/ManagerExpenseList.tsx
@@ -1,10 +1,10 @@
-import { type ExpenseListItems } from '@/api/manager/nexpense';
+import { type ExpenseListItems } from '@/api/manager/pexpense';
 
 import { Checkbox } from '@components/ui/checkbox';
 import { AppPagination } from '@/components/ui/AppPagination';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 
-import { ManagerListRow } from '@components/features/Expense/_components/ManagerListRow';
+import { ManagerListRow } from './_components/ManagerListRow';
 
 interface ExpenseListProps {
   loading: boolean;
@@ -40,17 +40,18 @@ export default function ManagerExpenseList({
       <Table variant="primary" align="center" className="table-fixed">
         <TableHeader>
           <TableRow className="[&_th]:px-2 [&_th]:text-[13px] [&_th]:font-medium">
-            <TableHead className="w-[8%]">EXP#</TableHead>
-            <TableHead className="w-[6%] whitespace-nowrap">증빙 수단</TableHead>
+            <TableHead className="w-[8%]">프로젝트#</TableHead>
+            <TableHead className="w-[5%] whitespace-nowrap">증빙 수단</TableHead>
             <TableHead className="w-[7%]">비용 용도</TableHead>
             <TableHead>비용 제목</TableHead>
-            <TableHead className="w-[5%] whitespace-nowrap">증빙 상태</TableHead>
+            <TableHead className="w-[5.5%] whitespace-nowrap">증빙 상태</TableHead>
+            <TableHead className="w-[5.5%] whitespace-nowrap">비용 유형</TableHead>
             <TableHead className="w-[9%]">금액</TableHead>
             <TableHead className="w-[8%]">세금</TableHead>
             <TableHead className="w-[9%]">합계</TableHead>
             <TableHead className="w-[7%]">작성자</TableHead>
-            <TableHead className="w-[7%]">상태</TableHead>
-            <TableHead className="w-[12%]">작성 일시</TableHead>
+            <TableHead className="w-[6%]">상태</TableHead>
+            <TableHead className="w-[8%]">작성일</TableHead>
             <TableHead className="w-[3%] px-0! transition-all duration-150">
               <Checkbox
                 id="chk_all"
@@ -65,13 +66,13 @@ export default function ManagerExpenseList({
         <TableBody>
           {loading ? (
             <TableRow>
-              <TableCell className="h-100 text-gray-500" colSpan={12}>
+              <TableCell className="h-100 text-gray-500" colSpan={13}>
                 비용 리스트 불러오는 중 . . .
               </TableCell>
             </TableRow>
           ) : expenseList.length === 0 ? (
             <TableRow>
-              <TableCell className="h-100 text-gray-500" colSpan={12}>
+              <TableCell className="h-100 text-gray-500" colSpan={13}>
                 리스트가 없습니다.
               </TableCell>
             </TableRow>
